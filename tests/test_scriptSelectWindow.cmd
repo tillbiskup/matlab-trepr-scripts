b99db2370ecdd3ab9b4a50cdeae8d08051a09435
Till Biskup <till@till-biskup.de>
2014-09-24 20:47:05 +0200

    Added test script for scriptSelectWindow display

---
+++ tests/test_scriptSelectWindow.cmd
@@ -0,0 +1,20 @@
+% TEST_SCRIPTSELECTWINDOW Testing the new CMD script selection GUI.
+%
+% This shall be a line with eighty characters length in total just to test.
+% Whether the window width is large enough... Let's see what happens.......
+%
+% The idea behind this is that Matlab(r) has a bit different ideas about
+% font sizes on different platforms, a fact partially accounted for in the 
+% code for the CMD script selection GUI textarea control.
+%
+% For this purpose, this "script" does just nothing besides having lots of
+% comment on top... that you're reading through in the moment - or even not
+% any more, who knows...
+%
+% The copyright part is of course a bit nonsensical in this case, but who 
+% cares. It is an example that even nonsensical content is no excuse for 
+% not having a proper copyright notice. Therefore never forget: Each and 
+% every script shall have such a copyright notice.
+
+% Copyright (c) 2014, Till Biskup
+% 2014-09-24
